8a44e537c6311e0878270e452953651ad4d148c8
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-02-19 06:38:45 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (82.66%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int cnt=1,ans=0;
-        bool flag=0;
-        vector<int> arr;
+        int cnt=1,ans=0,prev=0;
         for(int i=0;i<s.size()-1;i++){
             if(s[i]==s[i+1]){
                 cnt++;
             }
             else{
-                arr.push_back(cnt);
+                ans+=min(prev,cnt);
+                prev=cnt;
                 cnt=1;
             }
         }
-        arr.push_back(cnt);
-        for(int i=0;i<arr.size()-1;i++){
-            ans+=min(arr[i],arr[i+1]);
-        }
+        ans+=min(prev,cnt);
         return ans;
     }
 };
