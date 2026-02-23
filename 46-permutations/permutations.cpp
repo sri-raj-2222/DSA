1b36b4c4abf353b44f19fc8ab38715b7ec9d1cca
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-02-23 22:18:22 +0530

    Time: 3 ms (27.51%) | Memory: 11.2 MB (20.09%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    void dfs(int a,vector<int> &nums,set<vector<int>> & arr){
-        if(a > 1+nums.size()) return;
+    long long fact(long long n){
+        if(n==0 || n==1) return 1;
+        else return n*fact(n-1);
+    }
+    void dfs(long long a,vector<int> &nums,set<vector<int>> & arr){
+        if(arr.count(nums)) return;
         arr.insert(nums);
         for(int i=1;i<nums.size();i++){
             swap(nums[0],nums[i]);
-            dfs(a+1,nums,arr);
+            dfs(a,nums,arr);
             swap(nums[0],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         set<vector<int>> arr;
-        dfs(0,nums,arr);
+        long long a=fact(nums.size());
+        dfs(a,nums,arr);
         for(auto i : arr){
             ans.push_back(i);
         }
