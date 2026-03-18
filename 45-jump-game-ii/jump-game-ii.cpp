cd39c434384cbd809b88f18cc338c75d3b2c0c5c
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-03-18 23:40:41 +0530

    Time: 31 ms (24.25%) | Memory: 21.5 MB (20.22%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int dp[10000];
-    int fun(int i,vector<int> &nums){
-        if(i >= nums.size()) return 0;
-        if(i == nums.size()-1) return 0;
-        if(nums[i]==0) return 10000;
-        if(dp[i] != -1) return dp[i];
-        int k,mini=INT_MAX;
-        for(int j=1;j<=nums[i];j++){
-            k=fun(i+j,nums);
-            mini=min(k+1,mini);
-        }
-        return dp[i]=mini;
-    }
     int jump(vector<int>& nums) {
-        if(nums.size()==1) return 0;
-        memset(dp,-1,sizeof(dp));
-        int ans=fun(0,nums);
-        return ans;
+        int n=nums.size();
+        if(n==1) return 0;
+        vector<int> dp(n,INT_MAX);
+        dp[n-1]=0;
+        if(dp[n-2] > 0) dp[n-2]=1;
+        else dp[n-2] = INT_MAX;
+        for(int i=n-3;i>=0;i--){
+            if(nums[i] == 0){
+                dp[i]=INT_MAX;
+            }
+            int mini=INT_MAX;
+            for(int j=1;j<=nums[i];j++){
+                if(j+i < n) mini=min(mini,dp[j+i]);
+            }
+            if(mini != INT_MAX) dp[i]=1+mini;
+            else dp[i]=INT_MAX;
+        }
+        return dp[0];
     }
 };
