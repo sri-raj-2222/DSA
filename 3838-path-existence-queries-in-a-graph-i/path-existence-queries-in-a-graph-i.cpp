bcb7455a94d795a25f9f69103f9bb0679a23e2df
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-07-09 16:20:30 +0530

    Time: 210 ms (7.94%) | Memory: 297.2 MB (6.58%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        map<int,int> mpp;
+        unordered_map<int,int> mpp;
         mpp[0] = 0;
         for(int i=1;i<n;i++){
             if(nums[i] - nums[i-1] <= maxDiff){
