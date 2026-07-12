0817a56a9a83c0e18ddfd464817e513cc9782559
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-07-12 22:23:03 +0530

    Time: 21 ms (40.38%) | Memory: 10.9 MB (50.17%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    bool isPal(string s,int i,int j,vector<vector<int>> &dp){
-        if(i>=j) return dp[i][j] =  1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i] != s[j]) return dp[i][j] = 0;
-        return dp[i][j]= isPal(s,i+1,j-1,dp);
-    }
     int countSubstrings(string s) {
         int ans=0;
         int n=s.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<bool>> dp(n,vector<bool>(n,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s[i] == s[j]){
+                    if(j-i <= 1){
+                        dp[i][j] = true;
+                    }
+                    else dp[i][j] = dp[i+1][j-1];
+                }
+            }
+        }
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(isPal(s,i,j,dp)) ans++;
+                if(dp[i][j]) ans++;
             }
         }
         return ans;
