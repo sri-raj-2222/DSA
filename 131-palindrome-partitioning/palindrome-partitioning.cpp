e9a4d9a5a12776b1e7f7b9108452e3d6955469b4
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-07-12 19:57:23 +0530

    Time: 55 ms (44.81%) | Memory: 79.9 MB (29.81%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     vector<vector<string>>vct;
-    vector<vector<int>> dp;
-    bool pali(string &s, int index , int i){
-        if(dp[index][i] != -1) return dp[index][i];
-        int l=index;
-        int r=i;
-        while(index<=i){
-            if(s[index]!=s[i]){
-                return dp[l][r]=false;
-            }
-            index++;
-            i--;
-        }
-        return dp[l][r] = true;
-    }
-    void solve(int index,vector<string>&path,string& s)
-    {
-        if(index==s.size())
-        {
+    void solve(int index,vector<string>&path,string& s,vector<vector<bool>> &dp){
+        if(index==s.size()){
             vct.push_back(path);
             return ;
         }
-        for(int i=index;i<s.length();i++)
-        {
-            if(pali(s,index,i))
-            {
+        for(int i=index;i<s.length();i++){
+            if(dp[index][i]){
                 path.push_back(s.substr(index,i-index+1));
-
-                solve(i+1,path,s);
+                solve(i+1,path,s,dp);
                  path.pop_back();
             }
         }
@@ -36,9 +17,19 @@ public:
 
     vector<vector<string>> partition(string s) {
         int n=s.size();
-        dp.assign(n,vector<int>(n,-1));
+        vector<vector<bool>> dp(n,vector<bool>(n,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s[i] == s[j]){
+                    if(j-i <= 1){
+                        dp[i][j] = 1; 
+                    }
+                    else dp[i][j] = dp[i+1][j-1];
+                }
+            }
+        }
         vector<string>path;
-        solve(0,path,s);
+        solve(0,path,s,dp);
         return vct;
     }
 };
