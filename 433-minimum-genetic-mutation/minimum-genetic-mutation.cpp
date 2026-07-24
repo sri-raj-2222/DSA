4f1c757513af274eff4408d35aa12909f6954f5e
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-07-24 23:30:35 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (16.18%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -2,34 +2,29 @@ class Solution {
 public:
     int minMutation(string startGene, string endGene, vector<string>& bank) {
         queue<pair<string,int>> q;
-        map<string,int> mpp,vis;
+        unordered_map<string,int> mpp,vis;
         for(string i : bank){
             mpp[i] = 1;
         }
         char ch[4] = {'A','C','G','T'};
         q.push({startGene,0});
         mpp[startGene] = 1;
-        vis[startGene] = 1;
-        int ans=INT_MAX;
         while(!q.empty()){
             auto [str,d] = q.front();
             q.pop();
-            if(str == endGene){
-                ans = min(ans,d);
-            }
-            cout<<str<<" ";
+            if(str == endGene) return d;
             for(int i=0;i<8;i++){
                 char c=str[i];
                 for(int j=0;j<4;j++){
                     str[i] = ch[j];
-                    if(mpp[str] == 1 && vis[str] == 0){
+                    if(mpp[str] == 1 && mpp[str] != -1){
                         q.push({str,d+1});
-                        vis[str] = 1;
+                        mpp[str] = -1;
                     }
                 }
                 str[i]=c;
             }
         }
-        return ans == INT_MAX ? -1 : ans;
+        return -1;
     }
 };
