9fd7ba3bfd35aa159f101eddaaeeaa079faa79dd
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-07-24 22:27:08 +0530

    Time: 3 ms (87.61%) | Memory: 13.7 MB (53.51%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
+        priority_queue<
+        tuple<int,int,int>,
+        vector<tuple<int,int,int>>,
+        greater<tuple<int,int,int>>
+        > pq;
+        int row[4] = {0,0,1,-1};
+        int col[4] = {1,-1,0,0};
+        dist[0][0] = grid[0][0];
+        pq.push({0,0,grid[0][0]});
+        while(!pq.empty()){
+            auto [r,c,d] = pq.top();
+            pq.pop();
+            for(int i=0;i<4;i++){
+                int nr=r+row[i];
+                int nc=c+col[i];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n){
+                    int nd = max(d,grid[nr][nc]);
+                    if(dist[nr][nc] > nd){
+                        dist[nr][nc] = nd;
+                        pq.push({nr,nc,nd}); 
+                    }
+                }
+            }
+        }
+        return dist[n-1][n-1];
+
+    }
+};
