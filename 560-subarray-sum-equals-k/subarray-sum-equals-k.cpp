7d9d0af1f02a2122d11f803c187b0b508eb66d98
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-07-30 14:32:07 +0530

    Time: 49 ms (45.35%) | Memory: 52.5 MB (7.21%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -9,7 +9,7 @@ public:
         for(int i=1;i<n;i++){
             pre[i] = pre[i-1]+nums[i];
         }
-        map<int,int> mpp;
+        unordered_map<int,int> mpp;
         mpp[0] = 1;
         int ans = 0;
         for(int i=0;i<n;i++){
