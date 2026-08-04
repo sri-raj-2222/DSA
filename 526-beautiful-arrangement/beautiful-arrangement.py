fdb4f00e82d6cec0bbffba0d2b4b605dc6104bb7
Sri Raj Kumar <jangalasrirajkumar@gmail.com>
2026-08-04 22:39:48 +0530

    Time: 678 ms (32.93%) | Memory: 19.3 MB (49.44%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.py
+++ 526-beautiful-arrangement/beautiful-arrangement.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution:
     def __init__(self):
         self.ans = 0
     def solve(self,n,arr,nums,vis,j):
@@ -9,16 +9,12 @@ class Solution(object):
         for i in range(n):
             if vis[i] == 0 and (nums[i] % j ==  0 or j % nums[i] == 0):
                 vis[i] = 1
-                arr.append(nums[i]);
+                arr.append(nums[i])
                 self.solve(n,arr,nums,vis,j+1)
                 arr.pop()
                 vis[i] = 0
 
-    def countArrangement(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def countArrangement(self, n: int) -> int:
         self.ans = 0
         vis = [0] * n
         nums = [0] * n
